ebf21874dd3e6a64046b9aeebd32ae2f577a390a
pablo martin <114485473+paabloog@users.noreply.github.com>
2023-01-10 13:47:51 +0100

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -2,3 +2,30 @@ var numeros = [];
 for(var i=0; i<=10; i++){
 numeros . push(i);
 }
+var numero=3
+var texto = "tres"
+var nombreFrutas = ["fresa","melocotón","pera"];
+nombreFrutas.push ("limón")
+nombreFrutas.unshift("patata")
+nombreFrutas[2] = "higo"
+for(var i=0; i < nombreFrutas. length; i++){
+    alert(nombreFrutas[i]);
+}
+
+var j = 0
+while(  j < nombreFrutas.length   ){
+alertt(nombreFrutas[1]);
+}
+
+function seEencuentranEnListaElemento(lista, elemento)
+for(var i=0; i <lista, length; 1++){
+    if(lista[i] == elemento){
+        return true;
+    }
+}
+return false;
+
+function listaMenorElemento(lista){
+    lista.sort();
+    return lista.shift ()
+}
