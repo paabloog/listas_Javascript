203cfbfeb9f7c69bbf3b63832d51db80ffb21f58
pablo martin <114485473+paabloog@users.noreply.github.com>
2023-01-17 13:45:29 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -39,47 +39,6 @@
    var i = 0
    while(i < lista_comida.length){
    console.log(lista_comida[i])
-   i++ // Crea una lista de números pares del 2 al 20 utilizando un bucle while.
-
-   var lista_pares = [];
-   var i = 0;
-   while(i<=20){
-       if(i%2==0){
-       lista_pares.push(i);
-       }
-       i = i+1
-   }
-   console.log(lista_pares);
-
-   // Crea una lista de números impares del 1 al 19 utilizando un bucle for.
-
-   var lista_impares = [];
-   var i = 1;
-   while(i<=19){
-       if(i%2==1){
-       lista_impares.push(i);
-       }
-       i = i+1
-   }
-   console.log("impares:"+lista_impares);
-   
-   // Crea una lista de comida.
-
-   var lista_comida = ["pizza", "macarrones", "ensalada"];
-
-   // Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
-
-   lista_comida.unshift("pescado"),
-   
-   // Accede al segundo elemento de la lista de comida y cambia su valor por otro diferente.
-
-   lista_comida.splice(1, 1, "hamburguesa")
-   
-   // Recorre la lista de comida utilizando un bucle while y muestra cada elemento en la consola.
-
-   var i = 0
-   while(i < lista_comida.length){
-   console.log(lista_comida[i])
    i++
    }
 
@@ -91,24 +50,18 @@ function verultimoelementodelalista(lista){
    function suma(num1,num2){
       return num1+num2;
    }
-   // Crea una función que reciba una lista como parámetro y devuelva una nueva lista con los elementos de la lista original en orden inverso.
-   
-   // Crea una función que reciba una lista de números como parámetro y devuelva la suma de todos los números de la lista.
-   
-   // Crea una función que reciba una lista de números como parámetro y devuelva la media de todos los números de la lista.
-
+   // crea una función que nos devuelva si aparece un número en la lista o no
+function busqueda(lista, números){
+   for(var index=0; index <=lista.length;index++)
+   if(lista[index]==números){
+      return true;
    }
-
-   // Crea una función que reciba una lista como parámetro y devuelva el último elemento de la lista.
-function verultimoelementodelalista(lista){
-   return lista.length;
+   return false;
 }
-   // Crea una función que reciba una lista y un elemento como parámetros y agregue el elemento a la lista si no se encuentra en ella.
-   function suma(num1,num2){
-      return num1+num2;
-   }
+
    // Crea una función que reciba una lista como parámetro y devuelva una nueva lista con los elementos de la lista original en orden inverso.
    
    // Crea una función que reciba una lista de números como parámetro y devuelva la suma de todos los números de la lista.
    
    // Crea una función que reciba una lista de números como parámetro y devuelva la media de todos los números de la lista.
+
