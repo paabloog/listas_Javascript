e66cb6f2348809a9b840513f8f9a69650d1ea343
pablo martin <114485473+paabloog@users.noreply.github.com>
2023-01-17 13:19:53 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,31 +1,114 @@
-var numeros = [];
-for(var i=0; i<=10; i++){
-numeros . push(i);
-}
-var numero=3
-var texto = "tres"
-var nombreFrutas = ["fresa","melocotón","pera"];
-nombreFrutas.push ("limón")
-nombreFrutas.unshift("patata")
-nombreFrutas[2] = "higo"
-for(var i=0; i < nombreFrutas. length; i++){
-    alert(nombreFrutas[i]);
-}
+ // Crea una lista de números pares del 2 al 20 utilizando un bucle while.
 
-var j = 0
-while(  j < nombreFrutas.length   ){
-alertt(nombreFrutas[1]);
-}
+   var lista_pares = [];
+   var i = 0;
+   while(i<=20){
+       if(i%2==0){
+       lista_pares.push(i);
+       }
+       i = i+1
+   }
+   console.log(lista_pares);
 
-function seEencuentranEnListaElemento(lista, elemento)
-for(var i=0; i <lista, length; 1++){
-    if(lista[i] == elemento){
-        return true;
-    }
-}
-return false;
+   // Crea una lista de números impares del 1 al 19 utilizando un bucle for.
 
-function listaMenorElemento(lista){
-    lista.sort();
-    return lista.shift ()
+   var lista_impares = [];
+   var i = 1;
+   while(i<=19){
+       if(i%2==1){
+       lista_impares.push(i);
+       }
+       i = i+1
+   }
+   console.log("impares:"+lista_impares);
+   
+   // Crea una lista de comida.
+
+   var lista_comida = ["pizza", "macarrones", "ensalada"];
+
+   // Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
+
+   lista_comida.unshift("pescado"),
+   
+   // Accede al segundo elemento de la lista de comida y cambia su valor por otro diferente.
+
+   lista_comida.splice(1, 1, "hamburguesa")
+   
+   // Recorre la lista de comida utilizando un bucle while y muestra cada elemento en la consola.
+
+   var i = 0
+   while(i < lista_comida.length){
+   console.log(lista_comida[i])
+   i++ // Crea una lista de números pares del 2 al 20 utilizando un bucle while.
+
+   var lista_pares = [];
+   var i = 0;
+   while(i<=20){
+       if(i%2==0){
+       lista_pares.push(i);
+       }
+       i = i+1
+   }
+   console.log(lista_pares);
+
+   // Crea una lista de números impares del 1 al 19 utilizando un bucle for.
+
+   var lista_impares = [];
+   var i = 1;
+   while(i<=19){
+       if(i%2==1){
+       lista_impares.push(i);
+       }
+       i = i+1
+   }
+   console.log("impares:"+lista_impares);
+   
+   // Crea una lista de comida.
+
+   var lista_comida = ["pizza", "macarrones", "ensalada"];
+
+   // Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
+
+   lista_comida.unshift("pescado"),
+   
+   // Accede al segundo elemento de la lista de comida y cambia su valor por otro diferente.
+
+   lista_comida.splice(1, 1, "hamburguesa")
+   
+   // Recorre la lista de comida utilizando un bucle while y muestra cada elemento en la consola.
+
+   var i = 0
+   while(i < lista_comida.length){
+   console.log(lista_comida[i])
+   i++
+   }
+
+   // Crea una función que reciba una lista como parámetro y devuelva el último elemento de la lista.
+function verultimoelementodelalista(lista){
+   return lista.length;
 }
+   // Crea una función que reciba una lista y un elemento como parámetros y agregue el elemento a la lista si no se encuentra en ella.
+   function suma(num1,num2){
+      return num1+num2;
+   }
+   // Crea una función que reciba una lista como parámetro y devuelva una nueva lista con los elementos de la lista original en orden inverso.
+   
+   // Crea una función que reciba una lista de números como parámetro y devuelva la suma de todos los números de la lista.
+   
+   // Crea una función que reciba una lista de números como parámetro y devuelva la media de todos los números de la lista.
+
+   }
+
+   // Crea una función que reciba una lista como parámetro y devuelva el último elemento de la lista.
+function verultimoelementodelalista(lista){
+   return lista.length;
+}
+   // Crea una función que reciba una lista y un elemento como parámetros y agregue el elemento a la lista si no se encuentra en ella.
+   function suma(num1,num2){
+      return num1+num2;
+   }
+   // Crea una función que reciba una lista como parámetro y devuelva una nueva lista con los elementos de la lista original en orden inverso.
+   
+   // Crea una función que reciba una lista de números como parámetro y devuelva la suma de todos los números de la lista.
+   
+   // Crea una función que reciba una lista de números como parámetro y devuelva la media de todos los números de la lista.
